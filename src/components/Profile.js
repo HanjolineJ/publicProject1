b5969a7360d10e75a4b16b325fc50ebeb40c8a7d
HanjolineJ <Hanjolinej@gmail.com>
2025-03-31 19:56:59 -0400

    More styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HanProfessP1 from './HanProfessP1.jpeg'; // Import the image
+import HanProfessP1 from '../images/HanRowingP1.webp'; // Import the image
 
 function Profile() {
   return (
@@ -9,7 +9,16 @@ function Profile() {
       <img
         src={HanProfessP1}
         alt="Profile"
-        style={{ borderRadius: '100%' }}
+        style={{ borderRadius: '15%',
+        width: '200px', // Adjust the width as needed
+        height: '200px', // Adjust the height as needed
+        objectFit: 'cover', // Maintain aspect ratio
+        border: '2px solid #000', // Optional: Add a border
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Optional: Add shadow
+        marginBottom: '20px', // Optional: Add margin below the image
+        display: 'block', // Center the image
+         }}
+
       />
       <div className="mt-3">
         <p><strong>Name:</strong> Hanjoline Julceus </p>

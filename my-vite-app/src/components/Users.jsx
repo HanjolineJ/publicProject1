f06e98937cadbc567be997e20e0516478c03aaa0
HanjolineJ <Hanjolinej@gmail.com>
2025-04-12 10:13:04 -0400

    Yayyyy

--- my-vite-app/src/components/Users.jsx
+++ my-vite-app/src/components/Users.jsx
@@ -8,24 +8,40 @@ function Users() {
 
   useEffect(() => {
     fetch("http://138.197.99.80:2490/api/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data))
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Fetched users:", data); // Inspect returned data in the browser's console
+        setUsers(data);
+      })
       .catch((err) => console.error("Error loading users", err));
   }, []);
 
+  // Calculate pagination
   const totalPages = Math.ceil(users.length / perPage);
   const visibleUsers = users.slice((page - 1) * perPage, page * perPage);
 
   return (
     <div className="container mt-4">
       <h2>Users</h2>
+      {/* Display list of users */}
       <ul className="list-group">
         {visibleUsers.map((user) => (
-          <li key={user.id} className="list-group-item">
-            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          <li key={user._id} className="list-group-item">
+            {/* Display full name if available; fallback to user_name */}
+            <Link to={`/users/${user.user_name}`}>
+              {user.first_name && user.last_name
+                ? `${user.first_name} ${user.last_name}`
+                : user.user_name}
+            </Link>
           </li>
         ))}
       </ul>
+      {/* Numbered pagination */}
       <div className="mt-3">
         {Array.from({ length: totalPages }, (_, i) => (
           <button
